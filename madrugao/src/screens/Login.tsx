25f6c1cb76c3a49ecccbb65ee9dbaf9bfb4720ce
Rafael Leal Altero <rafa.altero@gmail.com>
2023-12-13 21:17:47 -0300

    up

--- madrugao/src/screens/Login.tsx
+++ madrugao/src/screens/Login.tsx
@@ -27,6 +27,7 @@ import { connect } from 'react-redux';
 import { add_On, setUser_login } from '../store/action/user';
 import Animated, { Easing, useAnimatedStyle, useSharedValue, withSpring, withTiming} from 'react-native-reanimated';
 import LottieView from 'lottie-react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 const animation = React.createRef<LottieView>();
 // updates
@@ -110,64 +111,162 @@ const LoginScreen = (props: any) => {
   }
   add_and_navegation();
   }, [props.user_on])
+  /////////////////////////////////////////////////////
 ///////////////////////////////////////reanimated///////////////////////////
   const config = {
-    duration: 300,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
+    duration: 650,
+    easing: Easing.elastic(1.2),
   };
-  const config_2 = {
-    mass: 1,
-    stiffness: 200,
-    damping: 7,
-  };
+  
 ///////////////////////////////////////reanimated///////////////////////////
   const position = useSharedValue(0);
 
+  const onPress_login = () => {
+    position.value = withTiming(Dimensions.get('window').width / 2, config);
+  };
+
+  const onPress_register = () => {
+    position.value = withTiming(-Dimensions.get('window').width / 2, config);
+  };
+
   const style2 = useAnimatedStyle(() => {
     return {
-      transform: [withSpring({ translateX: position.value })],
+      transform: [{ translateX: position.value }],
     };
   });
-
-  const onPress = () => {
-    position.value =  Dimensions.get('window').width / 2;
-  };
-  const onPress2 = () => {
-    position.value =  - Dimensions.get('window').width / 2;
-  };
+///////////////////////////////////////reanimated///////////////////////////
   const MyAnimatedImage = Animated.createAnimatedComponent(Image);
 
 ///////////////////////////////////////reanimated///////////////////////////
+  const [login,setLogin] = React.useState(false);
+  const [register,setRegister] = React.useState(false);
+  /////////////////////////////////////////////////////
+  useEffect(() => {
+    
 
+  }, [login,register]);
+  /////////////////////////////////////////////////////
   return (
     <SafeAreaView style={styles.container}>
+
       <View style={{justifyContent:'center',alignItems:'center'}}>
+        <Image source={require('../../assets/logos/logo_madrugao.png')} 
+        style={{margin:10,width:90,height:90,borderRadius:50}}
+        />
 
       </View>
+      {/* ////////////////////////login register ///////////////////////////////////// */}
+
       <View style={styles.contentContainer}>
-        <View style={styles.container_width}>
-          <TouchableOpacity style={styles.button} onPress={()=>onPress2()}>
-            <FontAwesome name="google-plus-square" size={35} color="#f4f7fc" />
-            <Text style={styles.buttonText}>Google</Text>
+        <View style={styles.container_width_logar}>
+            <TouchableOpacity style={[styles.button_logar, 
+                {
+                elevation: 7,
+                shadowColor: '#B20ACC',
+                }]} onPress={()=>{
+                  onPress_login()
+                  setRegister(false)
+                  setTimeout(()=>{
+                    setLogin(true)
+                  },200)
+                  }}>
+                  
+            <Text style={styles.buttonText_logar}>Login</Text>
+
           </TouchableOpacity>
-          
+                                 	
         </View>
-        <View style={styles.container_width2}>
-        <TouchableOpacity style={styles.button} onPress={()=>onPress()}>
-            <FontAwesome name="google-plus-square" size={35} color="#f4f7fc" />
-            <Text style={styles.buttonText}>Google</Text>
+        <View style={styles.container_width_logar}>
+        <TouchableOpacity style={[styles.button_logar,
+         {
+          elevation: 7,
+          shadowColor: '#0E00E3',}
+          ]} onPress={()=>{
+            onPress_register()
+            setLogin(false)
+            setTimeout(()=>{
+              setRegister(true)
+            },200)
+            
+            }}>
+            <Text style={styles.buttonText_logar}>Registrar</Text>
           </TouchableOpacity>
         </View>
       </View>
-
-
-      {/* <MyAnimatedImage 
-        source={require('../../assets/Home_2_p_t.png')} 
-        style={[styles.box,style2]}
-        resizeMode="contain"
-      /> */}
-       
       
+      {/* ////////////////////////buttons ///////////////////////////////////// */}
+
+      <View style={styles.contentContainer_buttons}>
+
+        <View style={styles.container_width_buttons}>
+          {login ?
+            <>
+              <Text style={styles.text}  numberOfLines={1} ellipsizeMode='tail'>Login</Text>
+
+              <View>
+                <TouchableOpacity style={[styles.button,{backgroundColor:"#f8fafd"}]} >
+
+                  <Image source={require('../../assets/logos/logo_google.png')} 
+                  style={{margin:1,width:30,height:30,}}
+                  />
+                  <Text style={[styles.buttonText,{color:'#202124'}]}>Google</Text>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.button, {borderColor:"#0E00E3",backgroundColor:"#2D2F31"}]} 
+                onPress={()=>Alert.alert('Em breve')}
+                >
+
+                  <Image source={require('../../assets/logos/logo_face.png')} 
+                  style={{margin:1,width:30,height:30}}
+                  
+                  />
+                  <Text style={styles.buttonText}>Facebook</Text>
+
+                </TouchableOpacity>
+              </View>
+            </>                       
+          :null}
+        </View>
+        
+        <View style={styles.container_width_buttons}>
+          {register ?
+            <>
+              <Text style={styles.text}  numberOfLines={1} ellipsizeMode='tail'>Registro</Text>
+              <View>
+                <TouchableOpacity style={[styles.button,{backgroundColor:"#f8fafd"}]} >
+
+                  <Image source={require('../../assets/logos/logo_google.png')} 
+                  style={{margin:1,width:30,height:30,}}
+                  />
+                  <Text style={[styles.buttonText,{color:'#202124'}]}>Google</Text>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.button, {borderColor:"#0E00E3",backgroundColor:"#2D2F31"}]} >
+
+                  <Image source={require('../../assets/logos/logo_face.png')} 
+                  style={{margin:1,width:30,height:30}}
+                  />
+                  <Text style={styles.buttonText}>Facebook</Text>
+
+                </TouchableOpacity>
+              </View>
+            </>
+          :null}
+        </View>
+
+      </View>
+
+      {/* ///////////////////////////////////////////////////////////// */}
+      <View pointerEvents="none" style={styles.box_view}>
+        <MyAnimatedImage 
+          source={require('../../assets/capa_login_7.png')} 
+          style={[styles.box,style2]}
+          resizeMode="contain"
+        />
+      </View>
+       
       {/* ///////////////////////////////////////////////////////////// */}
 
         {/* <Text style={styles.text}>Bem Vindo</Text> */}
@@ -183,9 +282,9 @@ const styles = StyleSheet.create({
   /////////////////////////////////////////////////////////////////
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor:'#2d2f31'
+    backgroundColor:'#202124'
   },
   contentContainer: {
     flexWrap: 'wrap',
@@ -193,19 +292,46 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between', // Isso colocará espaço máximo entre os filhos
     width: '100%', 
   },
-  container_width: {
+  
+  ////////////////////////////////////////// Logar e Registrar
+  container_width_logar: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:'#046bd3',
     width: '100%', 
-
   },
-  container_width2: {
-    flex: 1,
-    justifyContent: 'center',
+  button_logar: {
+    flexDirection:'row',
     alignItems: 'center',
-    backgroundColor:'#39cf2b',
+    justifyContent:'space-around',
+    
+    borderColor: '#f8fafd', // Cor do Google
+    borderWidth:0.5,
+    borderRadius: 15,
+
+    padding: 15,
+    margin: 10,
+    width: 150,
+    backgroundColor: '#202124', // Cor do Google
+  },
+  buttonText_logar: {
+    color: '#f4f7fc',
+    fontFamily:"Roboto-Regular",
+    fontSize:15,
+  },
+  ///////////////////////////////////////////////////////////////// buttons
+  contentContainer_buttons: {
+    flex:1,
+    flexWrap: 'wrap',
+    flexDirection: 'row', // Isso fará com que os filhos sejam dispostos em linha
+    justifyContent: 'space-between', // Isso colocará espaço máximo entre os filhos
+    width: '100%', 
+  },
+  
+  container_width_buttons: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
     width: '100%', 
   },
   /////////////////////////////////////////////////////////////////
@@ -213,27 +339,38 @@ const styles = StyleSheet.create({
     flexDirection:'row',
     alignItems: 'center',
     justifyContent:'space-around',
-    backgroundColor: '#4285F4', // Cor do Google
+
+    borderColor: '#f8fafd', // Cor do Google
+    borderWidth:0.5,
+    borderRadius: 15,
+
     padding: 15,
     margin: 10,
-    borderRadius: 15,
     width: 150,
   },
   buttonText: {
     color: '#f4f7fc',
-    fontWeight: 'bold',
+    fontFamily:"RobotoMono-Bold"
   },
   text :{
     color: '#f4f7fc',
-    fontSize:30,
+    fontSize: RFValue(40), // Ajuste o valor conforme necessário
     fontFamily:"OpenSans-Bold"
   },
   ////animacao
+  box_view: {
+    flex:1,
+    width:'100%',
+    height:'100%',
+    position: 'absolute',
+    top: 0,
+  },
   box: {
     flex:1,
     width:'100%',
     height:'100%',
     position: 'absolute',
+    top: 100,
   
   },
 });
